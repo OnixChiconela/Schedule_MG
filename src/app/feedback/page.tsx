29bf748d7d895ff43b45053ffdae77f80b2002be
OnixChiconela <josechiconela@icloud.com>
2025-05-09 21:00:35 +0200

    feedback

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -8,15 +8,14 @@ import { useTheme } from '../themeContext'
 export default function FeedbackPage() {
   const [feedback, setFeedback] = useState('')
   const [suggestionsFeedback, setSuggestionsFeedback] = useState('')
-  const {theme} = useTheme()
+  const { theme } = useTheme()
 
   const handleSubmit = () => {
     if (!feedback.trim()) {
-      toast.error('Por favor, insira seu feedback.')
+      toast.error(`Can't send know.`)
       return
     }
-    console.log('FeedbackPage: Submitted', { feedback, suggestionsFeedback })
-    toast.success('Obrigado pelo feedback!')
+    toast.success('!')
     setFeedback('')
     setSuggestionsFeedback('')
   }
@@ -24,19 +23,19 @@ export default function FeedbackPage() {
   return (
     <div className="p-6 min-h-screen bg-gray-100 dark:bg-slate-900">
       <h2 className={`text-xl font-semibold mb-4 ${theme === 'light' ? 'text-gray-900' : 'text-gray-100'}`}>
-        Enviar Feedback
+        Feedback
       </h2>
       <div className="max-w-md mx-auto">
-        <textarea
+        {/* <textarea
           value={feedback}
           onChange={(e) => setFeedback(e.target.value)}
-          placeholder="Seu feedback sobre a plataforma..."
+          placeholder="Give your feedback..."
           rows={4}
           className={`w-full p-3 mb-4 rounded-lg border ${
             theme === 'light' ? 'border-gray-300 bg-white text-gray-900' : 'border-slate-600 bg-slate-800 text-gray-200'
           } focus:outline-none focus:ring-2 focus:ring-blue-500`}
-        />
-        <textarea
+        /> */}
+        {/* <textarea
           value={suggestionsFeedback}
           onChange={(e) => setSuggestionsFeedback(e.target.value)}
           placeholder="O que achou das sugestões de eventos/projetos?"
@@ -44,17 +43,26 @@ export default function FeedbackPage() {
           className={`w-full p-3 mb-4 rounded-lg border ${
             theme === 'light' ? 'border-gray-300 bg-white text-gray-900' : 'border-slate-600 bg-slate-800 text-gray-200'
           } focus:outline-none focus:ring-2 focus:ring-blue-500`}
-        />
-        <motion.button
-          onClick={handleSubmit}
-          className={`w-full py-3 rounded-lg font-semibold ${
-            theme === 'light' ? 'bg-blue-500 hover:bg-blue-600 text-white' : 'bg-blue-600 hover:bg-blue-700 text-gray-100'
-          }`}
+        /> */}
+        <motion.a
+          href="mailto:josechiconela@icloud.com"
+          className={`w-full items-center justify-center flex py-3 cursor-pointer
+            rounded-lg font-semibold ${theme === 'light' ? 'bg-blue-500 hover:bg-blue-600 text-white' : 'bg-blue-600 hover:bg-blue-700 text-gray-100'
+            }`}
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
-          Enviar
-        </motion.button>
+          {/* <a
+            href="mailto:josechiconela@icloud.com"
+            className={`${theme === 'light'
+              ? 'text-gray-200 hover:text-fuchsia-600'
+              : 'text-neutral-200 hover:text-fuchsia-400'
+              } transition`}
+          >
+            Contact Us
+          </a> */}
+          Contact
+        </motion.a>
       </div>
     </div>
   )
