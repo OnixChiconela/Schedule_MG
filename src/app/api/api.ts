5defa6343a3ab7e7f6b535cd632b2f50ec80aede
OnixChiconela <josechiconela@icloud.com>
2025-05-22 23:37:57 +0200

    base

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: 'http://localhost:8050/api'
+    baseURL: process.env.NEXT_PUBLIC_FIND_BACKEND || 'http://localhost:8050/api'
 })
 
 export default api
